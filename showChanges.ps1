79083c5ddc4a2d2a4edffc3e0a506b43e37b18e2
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2025-01-17 15:18:46 -0600

    Update showChanges.ps1

--- showChanges.ps1
+++ showChanges.ps1
@@ -3,8 +3,6 @@ $mailboxes = get-mailbox -resultsize unlimited
 # Show what commands we would run
 foreach ($mailbox in $mailboxes) {
   $alias = $mailbox.alias
-  $cmd="Set-Mailbox `"" + $mailbox.alias + "`" -EmailAddresses @{add=`"" + $alias + "@guadalupetx.gov`"} -ErrorAction SilentlyContinue"
-  write-host $cmd
   $cmd2="Set-Mailbox `"" + $mailbox.alias + "`" -PrimarySMTPAddress `"" + $alias + "@guadalupetx.gov`" -EmailAddressPolicyEnabled:`$false -ErrorAction SilentlyContinue"
   write-host $cmd2
 }
