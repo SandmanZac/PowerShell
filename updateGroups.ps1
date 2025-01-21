41154b424a2d61d8f8337d244776a540598d580f
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2025-01-21 12:33:18 -0600

    Update updateGroups.ps1

--- updateGroups.ps1
+++ updateGroups.ps1
@@ -3,5 +3,7 @@ $groups = get-distributiongroup
 foreach ($group in $groups) {
     $alias = $group.Alias
     $newEmailAddress = $alias + "@guadalupetx.gov"
-    Set-DistributionGroup -Identity $group.Identity -EmailAddresses @{Add=$newEmailAddress} -EmailAddressPolicyEnabled:$false -primarySmtpAddress $newEmailAddress -ErrorAction SilentlyContinue
+    Set-DistributionGroup -Identity $group.Identity -EmailAddresses @{Add=$newEmailAddress} -ErrorAction SilentlyContinue
+    Set-DistributionGroup -Identity $group.Identity -EmailAddressPolicyEnabled:$false -ErrorAction SilentlyContinue
+    Set-DistributionGroup -Identity $group.Identity -primarySmtpAddress $newEmailAddress -ErrorAction SilentlyContinue
 }
