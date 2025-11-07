52bfe22faab96434270320cdd2f77493b9b0e84b
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2025-11-07 15:15:22 -0600

    Update Get-VirtualDirectories.ps1
    Removed the logic that was attempting to create the "Set" portion.

--- Get-VirtualDirectories.ps1
+++ Get-VirtualDirectories.ps1
@@ -1,66 +1,77 @@
-$server = op-p-hydra
+$Server = "ex01"
 
-#write-host "Get-ClientAccessService -Identity $server | select AutoDiscoverServiceInternalUri"
-$cas=Get-ClientAccessService -Identity $server | select AutoDiscoverServiceInternalUri
-#write-host "AutoDiscoverServiceInternalUri: "$cas.AutoDiscoverServiceInternalUri
-#write-host "To Revert:"
-write-host "Get-ClientAccessService -Server $server | Set-ClientAccessService -AutoDiscoverServiceInternalUri "$cas.AutoDiscoverServiceInternalUri
-#write-host ""
-#write-host "Get-EcpVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$ecp=Get-EcpVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $ecp.InternalUrl
-#write-host "ExternalURL: " $ecp.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-EcpVirtualDirectory -Server $server | Set-EcpVirtualDirectory -InternalURL "$ecp.InternalUrl" -ExternalURL "$ecp.ExternalUrl
-#write-host ""
-#write-host "Get-WebServicesVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$wsv=Get-WebServicesVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $wsv.InternalUrl
-#write-host "ExternalURL: " $wsv.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-WebServicesVirtualDirectory -Server $server | Set-WebServicesVirtualDirectory -InternalURL "$wsv.InternalUrl" -ExternalURL "$wsv.ExternalUrl
-#write-host ""
-#write-host "Get-MapiVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$mapi=Get-MapiVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $mapi.InternalUrl
-#write-host "ExternalURL: " $mapi.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-MapiVirtualDirectory -Server $server | Set-MapiVirtualDirectory -InternalURL "$mapi.InternalUrl" -ExternalURL "$mapi.ExternalUrl
-#write-host ""
-#write-host "Get-ActiveSyncVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$asv=Get-ActiveSyncVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $asv.InternalUrl
-#write-host "ExternalURL: " $asv.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-ActiveSyncVirtualDirectory -Server $server | Set-ActiveSyncVirtualDirectory -InternalURL "$asv.InternalUrl" -ExternalURL "$asv.ExternalUrl
-#write-host ""
-#write-host "Get-OabVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$oab=Get-OabVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $oab.InternalUrl
-#write-host "ExternalURL: " $oab.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-OabVirtualDirectory -Server $server | Set-OabVirtualDirectory -InternalURL "$oab.InternalUrl" -ExternalURL "$oab.ExternalUrl
-#write-host ""
-#write-host "Get-OwaVirtualDirectory -Server $server | select ExternalUrl,InternalUrl"
-$owa=Get-OwaVirtualDirectory -Server $server | select ExternalUrl,InternalUrl
-#write-host "InternalURL: " $owa.InternalUrl
-#write-host "ExternalURL: " $owa.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-OwaVirtualDirectory -Server $server | Set-OwaVirtualDirectory -InternalURL "$owa.InternalUrl" -ExternalURL "$owa.ExternalUrl
-#write-host ""
-#write-host "Get-PowerShellVirtualDirectory -Server $server | select Identity,ExternalUrl,InternalUrl"
-$psv=Get-PowerShellVirtualDirectory -Server $server | select Identity,ExternalUrl,InternalUrl
-#write-host "InternalURL: " $psv.InternalUrl
-#write-host "ExternalURL: " $psv.ExternalUrl
-#write-host "To Revert:"
-write-host "Get-PowerShellVirtualDirectory -Server $server | Set-PowerShellVirtualDirectory -InternalURL "$psv.InternalUrl" -ExternalURL "$psv.ExternalUrl
-#write-host ""
-#write-host "Get-OutlookAnywhere -Server $server | select ExternalHostname,InternalHostname,ExternalClientsRequireSsl,InternalClientsRequireSsl,DefaultAuthenticationMethod"
-$oav=Get-OutlookAnywhere -Server $server | select ExternalHostname,InternalHostname,ExternalClientsRequireSsl,InternalClientsRequireSsl,DefaultAuthenticationMethod
-#write-host "InternalHostname: " $oav.InternalHostname
-#write-host "InternalClientsRequireSsl: " $oav.InternalClientsRequireSsl
-#write-host "ExternalHostname: " $oav.ExternalHostname
-#write-host "ExternalClientsRequireSsl: " $oav.ExternalClientsRequireSsl
-#write-host "DefaultAuthenticationMethod: " $oav.DefaultAuthenticationMethod
-#write-host "To Revert:"
-write-host "Get-OutlookAnywhere -Server $server | Set-OutlookAnywhereDirectory -InternalURL "$oav.InternalHostname" -ExternalURL "$oav.ExternalHostname" -InternalClientsRequireSsl "$oav.InternalClientsRequireSsl" -ExternalClientsRequireSsl "$oav.ExternalClientsRequireSsl" -DefaultAuthenticationMethod "$oav.DefaultAuthenticationMethod
+if (Test-Path $env:ExchangeInstallPath\bin\RemoteExchange.ps1)
+{
+    . $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+    Connect-ExchangeServer -auto -AllowClobber
+}
+else
+{
+    Write-Warning "Exchange Server management tools are not installed on this computer."
+    EXIT
+}
+if ((Get-ExchangeServer $Server -ErrorAction SilentlyContinue).IsClientAccessServer)
+{
+	Write-Host "----------------------------------------"
+	Write-Host " Querying $Server"
+	Write-Host "----------------------------------------`r`n"
+	Write-Host "`r`n"
+
+	$AutoD = Get-ClientAccessServer $Server | Select AutoDiscoverServiceInternalUri
+	Write-Host "Autodiscover"
+	Write-Host " - Internal SCP: $($AutoD.AutoDiscoverServiceInternalUri)"
+	Write-Host "`r`n"
+
+	$ECP = Get-ECPVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "Exchange Control Panel"
+	Write-Host " - Internal: $($ECP.InternalURL)"
+	Write-Host " - External: $($ECP.ExternalURL)"
+	Write-Host "`r`n"
+
+	$EWS = Get-WebServicesVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "Exchange Web Services"
+	Write-Host " - Internal: $($EWS.InternalURL)"
+	Write-Host " - External: $($EWS.ExternalURL)"
+	Write-Host "`r`n"
+	
+	$MAPI = Get-MAPIVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "MAPI"
+	Write-Host " - Internal: $($MAPI.InternalURL)"
+	Write-Host " - External: $($MAPI.ExternalURL)"
+	Write-Host "`r`n"
+
+	$EAS = Get-ActiveSyncVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "ActiveSync"
+	Write-Host " - Internal: $($EAS.InternalURL)"
+	Write-Host " - External: $($EAS.ExternalURL)"
+	Write-Host "`r`n"
+
+	$OAB = Get-OABVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "Offline Address Book"
+	Write-Host " - Internal: $($OAB.InternalURL)"
+	Write-Host " - External: $($OAB.ExternalURL)"
+	Write-Host "`r`n"
+
+	$OWA = Get-OWAVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "Outlook Web App"
+	Write-Host " - Internal: $($OWA.InternalURL)"
+	Write-Host " - External: $($OWA.ExternalURL)"
+	Write-Host "`r`n"
+
+	$PS = Get-PowerShellVirtualDirectory -Server $Server -AdPropertiesOnly | Select InternalURL,ExternalURL
+	Write-Host "PowerShell"
+	Write-Host " - Internal: $($PS.InternalURL)"
+	Write-Host " - External: $($PS.ExternalURL)"
+	Write-Host "`r`n"
+
+	$OA = Get-OutlookAnywhere -Server $Server -AdPropertiesOnly | Select InternalHostName,ExternalHostName
+	Write-Host "Outlook Anywhere"
+	Write-Host " - Internal: $($OA.InternalHostName)"
+	Write-Host " - External: $($OA.ExternalHostName)"
+	Write-Host "`r`n"
+}
+else
+{
+	Write-Host -ForegroundColor Yellow "$Server is not a Client Access server."
+}
+Write-Host "Finished querying all servers specified."
